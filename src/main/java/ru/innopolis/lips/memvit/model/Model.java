46bff5332f26d5c9ec2cd63b306684be9b1d7655
Pavel Sozonov <pavel.sozonov@gmail.com>
2017-05-05 16:14:03 +0300

    Outcome file is updated

--- src/main/java/ru/innopolis/lips/memvit/model/Model.java
+++ src/main/java/ru/innopolis/lips/memvit/model/Model.java
@@ -5,17 +5,28 @@ import org.eclipse.cdt.debug.core.cdi.model.ICDIThread;
 /**
  * Contain all the plugin data (states, counters, states' flags, etc.)
  * Instantiated by the Controller
- *  
+ * 
  * @author Pavel Sozonov
  */
 public class Model {
 
+	// TODO: Add extended comment
+	// Updated by Debug Evetns Listener
+	private ICDIThread currentThread;
+
+	private boolean updatedThread = false;
+
 	// Count how many states are equals
 	private int duplicatedStates = 0;
-	
-	// State management buttons disable flag, when debug haven't started, using of buttons is disabled
+
+	// State management buttons disable flag, when debug haven't started, using
+	// of buttons is disabled
 	private boolean stateManagementButtonsEnabled = false;
-	
+
+	// Contain all the program execution states (each debug step is a separate
+	// state)
+	private StateStorage stateStorage = new StateStorage();
+
 	public boolean isStateManagementButtonsEnabled() {
 		return stateManagementButtonsEnabled;
 	}
@@ -25,12 +36,13 @@ public class Model {
 	}
 
 	/**
-	 * @param duplicatedStates the tempDuplicatedStates to set
+	 * @param duplicatedStates
+	 *            the tempDuplicatedStates to set
 	 */
 	public void setTempDuplicatedStates(int duplicatedStates) {
 		this.duplicatedStates = duplicatedStates;
 	}
-	
+
 	/**
 	 * @return the tempDuplicatedStates
 	 */
@@ -38,45 +50,38 @@ public class Model {
 		return duplicatedStates;
 	}
 
-	// Contain all the program execution states (each debug step is a separate state) 	
-	private StateStorage stateStorage = new StateStorage();
-	
 	/**
 	 * @return the stateStorage
 	 */
 	public StateStorage getStateStorage() {
 		return stateStorage;
 	}
-	
-	// TODO: Add extended comment
-	// Updated by Debug Evetns Listener
-	private ICDIThread currentThread;
 
-	private boolean updatedThread = false;
-	
 	/**
 	 * @return the currentThread
 	 */
 	public ICDIThread getCurrentThread() {
 		return currentThread;
 	}
-	
+
 	/**
-	 * @param currentThread the currentThread to set
+	 * @param currentThread
+	 *            the currentThread to set
 	 */
 	public void setCurrentThread(ICDIThread currentThread) {
 		this.currentThread = currentThread;
 	}
-	
+
 	/**
 	 * Setter for the updatedThread flag
 	 * 
-	 * @param value of the updatedThread flag
+	 * @param value
+	 *            of the updatedThread flag
 	 */
 	public void setUpdatedThread(boolean value) {
 		updatedThread = value;
 	}
-	
+
 	/**
 	 * Getter for the updatedThread
 	 * 
